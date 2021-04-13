7eb1ba7873413e122cac3a12e71c1a61499e70b8
Don van den Bergh <donvdbergh@hotmail.com>
2021-04-13 13:33:50 +0200

    Add action for generating translation files (#3)
    * started on an action for translations
    
    * updated files
    
    * fix run statement
    
    * try to actually execute generateLanguageFiles.sh
    
    * yet another try
    
    * yet another try
    
    * yet another try
    
    * don't add generated.txt
    
    * create bash script for pushing
    
    * bad fix
    
    * restructured the generateLanguageFiles a bit
    
    * debugging
    
    * first add, then check if anything changed
    
    * let's not make a new PR, this might just always work
    
    * change line endings
    
    * don't fail silently
    
    * update script to merge any open weblate PRs

---
+++ translations/generateLanguageFiles.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+MODULE_NAMES=${1:-"jaspTestModule"}
+PATH_TO_R_FILE=$2
+LANGUAGE_CODES=${3:-"nl de es pt ja tr"}
+
+create_file_if_it_doesnt_exist() {
+	if [ ! -f "$1" ]; then
+		touch "$1"
+	fi
+}
+
+do_lupdate()	{	lupdate -locations none -extensions cpp,qml -recursive $1 -ts $2;	}
+do_msgattrib()	{	msgattrib --no-obsolete --no-location ${1}/po/QML-${1}.pot -o $2;	}
+
+# in case people try this at home
+validate_module()
+{
+	if [ ! -f "$1/inst/qml" ] | [ ! -f "$1/R" ]; then
+		echo "either $1/inst/qml or $1/R doesn't exist, so $1 is probably not a JASP module."
+	fi
+}
+
+for moduleName in ${MODULE_NAMES[@]}
+do
+	echo Module: ${moduleName}
+
+	validate_module ${moduleName}
+
+	# ensure we don't crash if no translation infrastructure exists
+	mkdir -p "${moduleName}/po"
+	mkdir -p "${moduleName}/inst/qml/translations"
+
+	FILE=${moduleName}/po/QML-${moduleName}.pot
+
+	create_file_if_it_doesnt_exist $FILE
+	do_lupdate   $moduleName $FILE
+	do_msgattrib $moduleName $FILE
+
+	for languageCode in ${LANGUAGE_CODES[@]}
+	do
+		echo Generating language File: ${languageCode}
+		LANGUAGEFILE=${moduleName}/po/QML-${languageCode}.po
+
+		create_file_if_it_doesnt_exist $FILE
+		do_lupdate   $moduleName $LANGUAGEFILE
+		do_msgattrib $moduleName $LANGUAGEFILE
+
+		QMFILE=${moduleName}/inst/qml/translations/${moduleName}-${languageCode}.qm
+
+		lrelease $LANGUAGEFILE -qm $QMFILE
+
+	done
+	echo Rscript ${PATH_TO_R_FILE}/translate.R ${moduleName}
+	Rscript ${PATH_TO_R_FILE}/translate.R ${moduleName};
+done
---
+++ translations/updateUpstream.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Attempting to push any changed files."
+
+git config user.name github-actions
+git config user.email github-actions@github.com
+
+echo "$(git status)"
+
+git add "po" "inst/qml/translations" "*.po" "*.pot"
+git diff-index --quiet HEAD
+
+if [ $? = 0 ] ; then
+	echo "No translations files were updated, possibly only pushing a merged weblate PR ."
+fi
+
+# also print the commands for some nicer feedback
+echo "$(git status)"
+echo "$(git commit -m 'updated translation files')"
+echo "$(git status)"
+echo "$(git push)"
+echo "$(git status)"
+
+if [ $? = 0 ] ; then
+	echo "pushed directly to master."
+	exit 0;
+else
+	echo "pushing failed!"
+	exit 1;
+fi
