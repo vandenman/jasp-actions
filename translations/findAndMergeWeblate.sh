2f8574d62a7cbc786f44c7b6f1822c5eac4375dd
bruno boutin <b.boutin@uva.nl>
2021-04-22 13:52:56 +0200

    Update findAndMergeWeblate.sh

--- translations/findAndMergeWeblate.sh
+++ translations/findAndMergeWeblate.sh
@@ -45,7 +45,7 @@ case ${no_open_webplate_prs} in
 		git config user.email github-actions@github.com
 		echo "$(git status)"
 		echo "$(git fetch origin refs/pull/${pull_id}/head:PRBranch)"
-		echo "$(git checkout PRBranch)"
+		echo "$(git checkout PRBranch")
 		echo "$(git rebase origin/main")
 		# echo "$(git merge --squash --allow-unrelated-histories FETCH_HEAD)"
 		# echo "$(git commit)"
