6a69c48638b672a366128463aa6681cd4b133c16
bruno boutin <b.boutin@uva.nl>
2021-04-30 16:20:49 +0200

    Update findAndMergeWeblate.sh

--- translations/findAndMergeWeblate.sh
+++ translations/findAndMergeWeblate.sh
@@ -2,65 +2,27 @@
 
 owner_repo="$1"
 author='weblate'
+pr_number1=''
+pr_number2=''
 
-echo "Token is: $TOKEN"
-get_url="https://api.github.com/search/issues?q=is:pr+repo:${owner_repo}+author:${author}+state:open"
+pr_list=$(gh pr list -R $owner_repo -A $author)
+first_line=$(echo "$pr_list" | sed -n '1p')
+second_line=$(echo "$pr_list" | sed -n '2p')
+pr_number1=$(echo "$first_line" | grep -oE '^[0-9]+')
+pr_number2=$(echo "$second_line" | grep -oE '^[0-9]+')
 
-curl_reply=$(curl \
-	-H "Accept: application/vnd.github.v3+json" \
-	$get_url \
-)
-
-no_open_webplate_prs=$(echo "$curl_reply" | tr '\r\n' ' ' | jq '.total_count')
-if [ -z "${no_open_webplate_prs}" ] ; then
-	no_open_webplate_prs="-1"
+if [ -z "$pr_number1" ]
+then
+  echo "No Pull Request"
+else
+  echo "Pull Request ID: $pr_number1"
+  echo $(gh pr merge --squash $pr_number1 -R $owner_repo)
+  
+  if [ -z "$pr_number2" ]
+  then
+    echo "No other Pull Request"
+  else
+    echo "2nd Pull Request ID: $pr_number2"
+    echo $(gh pr merge --squash $pr_number2 -R $owner_repo)
+  fi
 fi
-
-if [ "${no_open_webplate_prs}" = "null" ] ; then
-	echo "Error: did not get an expected reply from the github api. This probably requires human intervention. The response was:"
-	echo
-	echo "${curl_reply}"
-	exit 1
-fi
-
-echo
-case ${no_open_webplate_prs} in
-	-1)
-
-		echo "Error: did not get an expected reply from the github api. This probably requires human intervention. The response was:"
-		echo "${curl_reply}"
-		exit 1;;
-
-	0)
-		echo "found no open weblate PRs, aborting early."
-		# TODO: we could abort the entire check early now but exiting with nonzero causes the check to fail.
-		exit 0;;
-
-	1)
-		pull_id=$(echo "$curl_reply" | tr '\r\n' ' ' | jq '.items[0].number')
-
-		echo "found 1 open weblate PR with number ${pull_id}. Merging it locally."
-
-		git config user.name github-actions
-		git config user.email github-actions@github.com
-		echo "$(git status)"
-		put_url="https://api.github.com/repos/${owner_repo}/pulls/${pull_id}/merge"
-		echo "${put_url}"
-		curl_reply=$(curl \
-		        -X PUT \
-		        $put_url \
-		        -d '{"commit_title":"myCommit", "merge_method": "squash"}' \
-        		-H "Authorization: Bearer $TOKEN")
-		echo "$curl_reply"
-		
-		#echo "$(git fetch origin refs/pull/${pull_id}/head:PRBranch)"
-		#echo "$(git merge --squash PRBranch)"
-
-		exit 0;;
-
-	*)
-		echo "Found 2 or more open weblate PRs. This probably requires human intervention. The response was:"
-		echo "$curl_reply"
-		exit 1;;
-
-esac
