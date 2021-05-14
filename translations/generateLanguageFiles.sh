b8339cededd338bbe2b5be4102c909e84aed2a34
bruno boutin <b.boutin@uva.nl>
2021-05-14 22:52:41 +0200

    Update generateLanguageFiles.sh

--- translations/generateLanguageFiles.sh
+++ translations/generateLanguageFiles.sh
@@ -2,7 +2,7 @@
 
 MODULE_NAMES=${1:-"jaspTestModule"}
 MODULE_NAMES=$(echo $MODULE_NAMES | cut -d'/' -f 2)
-PATH_TO_R_FILE=$2
+PATH_TO_R_FILE=$PWD
 LANGUAGE_CODES=${3:-"nl de es pt ja tr"}
 
 create_file_if_it_doesnt_exist() {
