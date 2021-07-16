81ba49b346c542610382deafe296f1cbeb710256
bruno boutin <b.boutin@uva.nl>
2021-07-16 22:37:03 +0200

    Update lockWeblate.sh

--- testwlc/lockWeblate.sh
+++ testwlc/lockWeblate.sh
@@ -1,12 +1,14 @@
 lock_repo() {
-  if [ -z "$1" ]
-  then 
-    echo "No $3 Weblate repo"
-  else
-    wlc --key $2 --url https://hosted.weblate.org/api/ lock jasp/$1
+   echo "lock $1"
+   wlc --key $2 --url https://hosted.weblate.org/api/ lock jasp/$1
 }
 
-lock_repo $QML_REPO $WEBLATE_KEY "QML"
+if [ "$QML_REPO" ]
+then
+  lock_repo $QML_REPO $WEBLATE_KEY
+fi
 
-lock_repo $R_REPO $WEBLATE_KEY "R"
-
+if [ "$R_REPO" ]
+then
+  lock_repo $R_REPO $WEBLATE_KEY
+fi
