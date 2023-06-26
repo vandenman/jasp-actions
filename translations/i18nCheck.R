0bf49a1b44af4d355b1c94de038a4fe53b2a1c58
Shun Wang <shuonwang@gmail.com>
2023-06-26 21:29:32 +0800

    Add err check for placeholders and po compile (#25)
    * Add err check for placeholders and po compile
    
    * fix typo
    
    Co-authored-by: Don van den Bergh <donvdbergh@hotmail.com>
    
    ---------
    
    Co-authored-by: Don van den Bergh <donvdbergh@hotmail.com>

--- translations/i18nCheck.R
+++ translations/i18nCheck.R
@@ -9,18 +9,44 @@ checkStatus <- c()
 cli_h1("Check R translations:")
 rPotData <- potools::get_message_data(dir = ".", verbose = TRUE)
 
-# Get wrong usage of gettext from .R
-rErrorCalls <- subset(rPotData, grepl(pattern="gettext(|f)\\(['\"]['\"]\\)", call), select = c("file", "call", "line_number"))
-          
-if (nrow(rErrorCalls) > 0) {
+# Get unreasonable usage of gettext from .R
+placeholderData <- subset(rPotData,
+                          grepl(pattern = "(.*%[a-zA-Z].*|.*%[0-9]*[a-zA-Z].*){2,}", call) & !grepl(pattern = "%%", call),
+                          select = c("file", "call", "line_number"))
+rEmptyCalls <- subset(rPotData,
+                      grepl(pattern = "gettext(|f)\\(['\"]['\"]\\)", call),
+                      select = c("file", "call", "line_number"))
+
+# Get po/mo compiling error
+e <- capture.output(tools::update_pkg_po("."))
+
+if (length(e) > 0) {
   checkStatus <- c(checkStatus, 1)
-  # Warning in CLI
-  cli_alert_danger("{nrow(rErrorCalls)} empty gettext call(s) found")
+  # Format data to make read friendly in Github CL
+  rPoError <- as.data.frame(matrix(e, ncol=5, byrow=TRUE))[1:4]
+  colnames(rPoError) <- c("Error_Location", "Error_Type", "Original_Gettext", "Translated_text")
+  cli_alert_danger("some translation error found in po file")
+  cli_h2("Please request translation maintainer or refer to following to resolve them:")
+  print(t(rPoError))
+}
+
+if (nrow(rEmptyCalls) > 0) {
+  checkStatus <- c(checkStatus, 1)
+  cli_alert_danger("{nrow(rEmptyCalls)} empty gettext call(s) found")
   cli_h2("Please refer to following to resolve them:")
-  print(rErrorCalls, row.names = FALSE)
-  } else {
-    cli_alert_success("R message check PASSED")
-} 
+  print(rEmptyCalls, row.names = FALSE)
+}
+
+if (nrow(placeholderData) > 0) {
+  checkStatus <- c(checkStatus, 1)
+  cli_alert_danger("{nrow(placeholderData)} multiple placeholders without index found")
+  cli_h2("Please refer to following to resolve them:")
+  print(placeholderData, row.names = FALSE)
+}
+
+if (length(checkStatus) == 0) {
+  cli_alert_success("R message check PASSED")
+}
 
 cli_h1("Check QML translations:")
 # Get QML files paths
@@ -28,26 +54,27 @@ qmlFiles <- list.files(path = file.path("inst", "qml"), pattern = "\\.qml", recu
 
 if (length(qmlFiles) == 0) {
   cli_alert("No QML file found")
-  } else {
+} else {
   # Generate pot meta data from QML
   qmlSrcData <- data.frame()
   message("Getting QML-level messages...")
-
+  
   for (i in 1:length(qmlFiles)) {
     filePaths <- paste0("./", qmlFiles[i])
-    readL     <- as.data.frame(readLines(filePaths, warn = FALSE))
-    tempData  <- data.frame(
-      Source_file      = rep(filePaths), 
-      Call_code        = readL[,1], 
+    readL <- as.data.frame(readLines(filePaths, warn = FALSE))
+    tempData <- data.frame(
+      Source_file      = rep(filePaths),
+      Call_code        = readL[, 1],
       Line_number      = 1:nrow(readL),
-      Translation_call = grepl(pattern="qsTr(|Id|anslate)\\(\".*\"\\)", readL[,1]),
-      Empty_call       = grepl(pattern="qsTr(|Id|anslate)\\(\"\"\\)", readL[,1]))
-
+      Translation_call = grepl(pattern = "qsTr(|Id|anslate)\\(\".*\"\\)", readL[, 1]),
+      Empty_call       = grepl(pattern = "qsTr(|Id|anslate)\\(\"\"\\)", readL[, 1])
+    )
+    
     qmlSrcData <- rbind(qmlSrcData, tempData)
   }
-
+  
   qmlErrorCalls <- subset(qmlSrcData, qmlSrcData$Empty_call == 1, select = c(1:3))
-
+  
   if (nrow(qmlErrorCalls) > 0) {
     checkStatus <- c(checkStatus, 1)
     cli_alert_danger("{nrow(qmlErrorCalls)} empty Qt translate call(s) found")
@@ -56,11 +83,11 @@ if (length(qmlFiles) == 0) {
   } else {
     cli_alert_success("QML message check PASSED")
   }
-} 
+}
 
-if (length(checkStatus) > 0){
+if (length(checkStatus) > 0) {
   # failing github action,custom exit code to different from a system exit
   quit(status = 10)
-} else{
+} else {
   cli_alert_success("All i18n check PASSED")
 }
