92633c874954628cfde2a49efcfe8e69f48fdd34
Shun Wang <shuonwang@gmail.com>
2024-01-11 15:38:32 +0800

    Add check for multiple placeholders numbers not start from 1 (#34)
    * Add check for multiple placeholders numbers not start from 1
    
    * Match numbering error with multiple placeholders
    
    Also check if `%s` and `%1$s` simultaneously exist.
    
    * Typo

--- translations/i18nCheck.R
+++ translations/i18nCheck.R
@@ -5,6 +5,15 @@ library(potools)
 library(cli)
 
 checkStatus <- c()
+
+msgErrorCheck <- function(msgData, warningMsg) {
+  if (nrow(msgData) > 0) {
+    checkStatus <<- c(checkStatus, 1)
+    cli_h2(warningMsg)
+    cli_alert_danger("Please refer to following to resolve them:")
+    print(msgData, row.names = FALSE)
+  }
+}
 # Generate pot meta data from .R
 cli_h1("Check R translations:")
 rPotData <- potools::get_message_data(dir = ".", verbose = TRUE)
@@ -12,40 +21,26 @@ rPotData <- potools::get_message_data(dir = ".", verbose = TRUE)
 # Get unreasonable usage of gettext from .R
 placeholderData <- subset(rPotData,
                           grepl(pattern = "(.*%[a-zA-Z].*|.*%[0-9]*[a-zA-Z].*){2,}", msgid)  # match multiple placeholders > 2 times.
-                            & !grepl(pattern = "%%", msgid) # such '90%%' cases in gettextf
-                            |  grepl(pattern = "(.*%[a-zA-Z].*|.*%[0-9]*[a-zA-Z].*){3,}", msgid_plural), # match plural conditions (> 3 times?)
+                          & !grepl(pattern = "%%", msgid) # such '90%%' cases in gettextf
+                          |  grepl(pattern = "(.*%[a-zA-Z].*|.*%[0-9]*[a-zA-Z].*){3,}", msgid_plural), # match plural conditions (> 3 times?)
                           select = c("file", "call", "line_number"))
 rEmptyCalls <- subset(rPotData,
                       grepl(pattern = "gettext(|f)\\(['\"]['\"]\\)", call),
                       select = c("file", "call", "line_number"))
+templateMsgError <- subset(rPotData, grepl(pattern = "%\\d\\$", msgid)                                 # match all placeholders like %1$s
+                            & (!grepl(pattern = "%1\\$", msgid) | grepl(pattern = "%[a-zA-Z]", msgid)), # match missing %1$ or %s is present
+                           select = c("file", "call", "line_number"))
 
-# Get po/mo compiling error
+# Get po/mo compiling error of R
 e <- capture.output(tools::update_pkg_po("."))
+rPoError <- as.data.frame(matrix(e, ncol=5, byrow=TRUE))[1:4]
+colnames(rPoError) <- c("Error_Location", "Error_Type", "Original_Gettext", "Translated_text")
 
-if (length(e) > 0) {
-  checkStatus <- c(checkStatus, 1)
-  # Format data to make read friendly in Github CL
-  rPoError <- as.data.frame(matrix(e, ncol=5, byrow=TRUE))[1:4]
-  colnames(rPoError) <- c("Error_Location", "Error_Type", "Original_Gettext", "Translated_text")
-  cli_alert_danger("some translation error found in po file")
-  cli_h2("Please request translation maintainer or refer to following to resolve them:")
-  print(t(rPoError))
-}
-
-if (nrow(rEmptyCalls) > 0) {
-  checkStatus <- c(checkStatus, 1)
-  cli_alert_danger("{nrow(rEmptyCalls)} empty gettext call(s) found")
-  cli_h2("Please refer to following to resolve them:")
-  print(rEmptyCalls, row.names = FALSE)
-}
-
-if (nrow(placeholderData) > 0) {
-  checkStatus <- c(checkStatus, 1)
-  cli_alert_danger("{nrow(placeholderData)} multiple placeholders without index found")
-  cli_h2("Please refer to following to resolve them:")
-  print(placeholderData, row.names = FALSE)
-}
-
+msgErrorCheck(rPoError,         "Some translation errors found in po file")
+msgErrorCheck(rEmptyCalls,      "{nrow(rEmptyCalls)} empty gettext call(s) found")
+msgErrorCheck(placeholderData,  "{nrow(placeholderData)} multiple placeholders without index found")
+msgErrorCheck(templateMsgError, "There are numbering errors with multiple placeholders")
+  
 if (length(checkStatus) == 0) {
   cli_alert_success("R message check PASSED")
 }
@@ -75,16 +70,13 @@ if (length(qmlFiles) == 0) {
     qmlSrcData <- rbind(qmlSrcData, tempData)
   }
   
-  qmlErrorCalls <- subset(qmlSrcData, qmlSrcData$Empty_call == 1, select = c(1:3))
-  
-  if (nrow(qmlErrorCalls) > 0) {
-    checkStatus <- c(checkStatus, 1)
-    cli_alert_danger("{nrow(qmlErrorCalls)} empty Qt translate call(s) found")
-    cli_h2("Please refer to following to resolve them:")
-    print(qmlErrorCalls, row.names = FALSE)
-  } else {
-    cli_alert_success("QML message check PASSED")
-  }
+  qmlErrorCalls <- subset(qmlSrcData, qmlSrcData$Empty_call == 1, select = c(1:3)) 
+}
+
+if (nrow(qmlErrorCalls) == 0) {
+  cli_alert_success("QML message check PASSED")
+} else {
+  msgErrorCheck(qmlErrorCalls, "{nrow(qmlErrorCalls)} empty Qt translate call(s) found")
 }
 
 if (length(checkStatus) > 0) {
