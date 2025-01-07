5545995234589205d2c2ded85ffead9aa099cfc7
bruno boutin <b.boutin@uva.nl>
2025-01-07 12:01:11 +0100

    Update i18nCheck.R

--- translations/i18nCheck.R
+++ translations/i18nCheck.R
@@ -39,7 +39,7 @@ rErrorCalls <- subset(rPotData,
 jaspCheckPoFiles <- function(dir) {
   files <- list.files(path = dir, pattern = "^R-.*[.]po$",
                       full.names = TRUE, recursive = TRUE)
-  files <- files[!endsWith(files, "ta.po")]
+  #files <- files[!endsWith(files, "ta.po")]
   result <- matrix(character(), ncol = 5L, nrow = 0L)
   for (f in files) {
     errs <- tools::checkPoFile(f, strictPlural = startsWith(basename(f), "R-"))
